404461c7ad121206cda0a9c2b964e9bd8fa5ccd2
Your Name <you@example.com>
2023-11-14 22:08:38 +0000

    update

--- start_browser.sh
+++ start_browser.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+export DISPLAY=:0 
 xset -dpms # Disable DPMS (Energy Star) features.
 xset s off # Disable screen saver.
 xset s noblank # Don't blank the video device.
-unclutter -idle 0
+unclutter -idle 0 &
 matchbox-window-manager &
 # Start Midori in fullscreen mode with the specified URL
 midori -e Fullscreen  http://localhost:5000 &
@@ -11,4 +12,8 @@ midori -e Fullscreen  http://localhost:5000 &
 sleep 25
 
 # Simulate pressing the Tab key
+xdotool key Tab
+
+sleep 100
+
 xdotool key Tab
