0e46ff22c51692e8970001721713a0d65672a301
Your Name <you@example.com>
2023-11-14 21:40:08 +0000

    update

--- start_browser.sh
+++ start_browser.sh
@@ -1,12 +1,14 @@
-#sudo -u canhho epiphany-browser -a -i --profile ~/.config http://localhost:5000 --display=:0 &
-#sleep 15s;
-#xte "key F11" -x:0
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
-
-unclutter &
+#!/bin/sh
+xset -dpms # Disable DPMS (Energy Star) features.
+xset s off # Disable screen saver.
+xset s noblank # Don't blank the video device.
+unclutter -idle 0
 matchbox-window-manager &
-midori -e Fullscreen -a http://localhost:5000
-sleep 15s;
-xte "key F11" -x:0
+# Start Midori in fullscreen mode with the specified URL
+midori -e Fullscreen  http://localhost:5000 &
+
+# Wait for Midori to load the webpage
+sleep 25
+
+# Simulate pressing the Tab key
+xdotool key Tab
