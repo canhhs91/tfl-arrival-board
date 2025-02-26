7b9195d889f6f289bc67c4198e6f087f8e0c88fe
Canh Ho <>
2025-02-26 14:06:34 +0000

    update

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import LedContent from "./components/led-content";
 
 export default function Home() {
   return (
     <div className="led-matrix">
       <div className="text text-regular">
-        <LedContent />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LedContent />
+        </Suspense>
       </div>
     </div>
   );
